03f7939fce5ed8229cbb93b61f2a5f2318632007
Xzzit <897177017@qq.com>
2023-02-06 17:04:00 +0900

    add comments and exercises

--- 01_HelloWorld/main.cu
+++ 01_HelloWorld/main.cu
@@ -13,7 +13,20 @@ int main()
 
     // call a kernel
     cuda_hello<<<1,1>>>(); 
+
+    /*
+	 Synchronize with GPU to wait for printf to finish.
+	 Results of printf are buffered and copied back to
+	 the CPU for I/O after the kernel has finished.
+	*/
     cudaDeviceSynchronize();
 
     return 0;
 }
+
+/*
+Exercises:
+1) Change the message that is printed by the kernel
+2) Write a different kernel (different name, different message)
+3) Call the different kernels multiple times
+*/
